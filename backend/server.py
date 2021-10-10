664c8da4db7e88e5b33c6cececed209a4afd6929
TrinaChang <ks20342@gmail.com>
2021-10-10 23:49:55 +0800

    add flask functions

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from flask_cors import CORS
+from json import dumps
+
+from auth_register import auth_register
+from auth_login import auth_login
 
 def default_handler(err):
     ''' Default Handle '''
@@ -11,9 +15,29 @@ APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, default_handler)
 
 
+@APP.route("/auth/register", methods=['POST'])
+def auth_register_root():
+    ''' Register User '''
+    payload = request.get_json()
+    first_name = payload['first_name']
+    last_name = payload['last_name']
+    email = payload['email']
+    address = payload['address']
+    state = payload['state']
+    postcode = payload['postcode']
+    phone = payload['phone']
+    username = payload['username']
+    password = payload['password']
+    confirmpassword = payload['confirmpassword']
+    return dumps(
+        auth_register(first_name, last_name, email, address, state, postcode, phone, username, password, confirmpassword)
+    )
+
 @APP.route("/auth/login", methods=['POST'])
 def auth_login_root():
-    pass
+    ''' Login a user '''
+    payload = request.get_json()
+    return dumps(auth_login(payload['username'], payload['password']))
 
 if __name__ == "__main__":
     pass
