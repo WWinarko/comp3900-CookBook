f390c550adebd0a8f1f5f9ebfc9c24d65006b84c
Louis Limmarga <louislimmarga@gmail.com>
2021-10-11 23:13:05 +1100

    fix propvalidation

--- frontend/cookbook/src/components/AddStep.jsx
+++ frontend/cookbook/src/components/AddStep.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import CustomTextField from "../components/TextField/CustomTextField";
 import  { AddButton } from "../pages/AddRecipe";
 
-function AddStep(steps, setSteps, setNewStep) {
+function AddStep({ steps, setSteps, setNewStep }) {
   const [step, setStep] = useState('');
   
   const handleSave = () => {
@@ -18,6 +20,12 @@ function AddStep(steps, setSteps, setNewStep) {
       <AddButton onClick={handleSave}>Save</AddButton>
     </>
   )
+}
+
+AddStep.propTypes = {
+  steps: PropTypes.object,
+  setSteps: PropTypes.func,
+  setNewStep: PropTypes.func
 }
 
 export default AddStep;
