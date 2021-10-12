e5a9858c3329f0befae90134ba25c22099340a77
TrinaChang <ks20342@gmail.com>
2021-10-12 18:20:42 +0800

    recipe view/upload finished

---
+++ backend/recipe_upload.py
@@ -0,0 +1,85 @@
+import argument_checker
+import token_helper
+import database
+from error import InputError
+
+def recipe_upload(token, title, intro, photo, difficulty, cooktime, preptime, serves, ingredients, steps):
+    users = database.get_users()
+
+    # check if the token is valid
+    token_helper.is_token_valid(token, users)
+
+    # check if the fields have arguments
+    argument_checker.all_not_empty([title, intro, photo])
+    
+    # check if no input
+    if (difficulty is None) or (cooktime is None) or (preptime is None) or (serves is None):
+        raise InputError(description="no input for any of diffculty/cooktime/preptime/serves")
+
+    # no empty step
+    argument_checker.all_not_empty(steps)
+
+    for ingredient in ingredients:
+        if argument_checker.is_empty_string(ingredient['ingredient']):
+            raise InputError(description="contains empty ingredient textfield")
+
+    user = users.find_one({"token":token})
+    owner_id = str(user['_id'])
+    recipe = {
+        "title":title,
+        "intro":intro,
+        "photo":photo,
+        "sold":0,
+        "difficulty":difficulty,
+        "rating":0,
+        "people_rated":0,
+        "owner_id":owner_id,
+        "cooktime":cooktime,
+        "preptime":preptime,
+        "serves":serves,
+        "ingredients": ingredients,
+        "steps":steps
+    }
+
+    recipes = database.get_recipes()
+    result = recipes.insert_one(recipe)
+
+    return {
+        'recipe_id': str(result.inserted_id)
+    }
+
+# users = database.get_users()
+# user = users.find_one({"username":"wincent"})
+
+# recipe_upload(
+#     user['token'], 'testing', 'dfsfsd', 'sfssd', 5, 30, 20, 4, 
+#     [{'ingredient':'dfsfsdfsdf', 'product_id':'fsdfsfd'}, {'ingredient':'abcsdf', 'product_id':'adslsfd'}], 
+#     ['sdfdsfsdfs', 'sfdfsdfsdfsfds', 'fsdfsdfsdsfdsfs'])
+
+'''
+recipe format:
+{
+    'title': 'sdfsfsds' (strip, not empty)
+    'intro':
+    'photo':
+    'sold':
+    'difficulty':
+    'rating':
+    'people_rated':
+    'owner_id':
+    'cooktime':
+    'preptime':
+    'serves':
+    'ingredients':
+        [
+            {
+                'ingredient':
+                'product_id':
+            }
+        ]
+    'steps':
+        [
+            
+        ]
+}
+'''
---
+++ backend/recipe_view.py
@@ -0,0 +1,99 @@
+import database
+import recipe_helper
+import auth_helper
+from error import AccessError
+from bson.objectid import ObjectId
+
+def recipe_view(recipe_id):
+    ''' view a recipe '''
+    recipes = database.get_recipes()
+
+    # check if the recipe exists
+    if not recipe_helper.check_recipe_exist(recipe_id, recipes):
+        raise AccessError(description="recipe does not exist")
+    
+    recipe = recipes.find_one({"_id":recipe_id})
+
+    title = recipe['title']
+    intro = recipe['intro']
+    photo = recipe['photo']
+    sold = recipe['sold']
+    difficulty = recipe['difficulty']
+    rating = recipe['rating']
+    cooktime = recipe['cooktime']
+    preptime = recipe['preptime']
+    serves = recipe['serves']
+    ingredients = recipe['ingredients']
+    ingredient_string = []
+    for ingredient in ingredients:
+        ingredient_string.append(ingredient['ingredient'])
+    steps = recipe['steps']
+
+    users = database.get_users()
+    owner_id = recipe['owner_id']
+
+    # check if the user exists
+    if not auth_helper.check_user_id(ObjectId(owner_id), users):
+        raise AccessError(description="user does not exist")
+    
+    owner = users.find_one({"_id":ObjectId(owner_id)})
+    
+    # if the owner is admin no follower numbershows up
+    if owner['admin']:
+        owner_username = "admin"
+        owner_follower = -1
+        owner_photo = ""
+    else:
+        owner_username = owner['username']
+        owner_follower = owner['follower']
+        owner_photo = owner['photo']
+
+    # return the recipe details
+    return {
+        'title': title,
+        'intro': intro,
+        'photo': photo,
+        'sold': sold,
+        'difficulty': difficulty,
+        'rating': rating,
+        'cooktime': cooktime,
+        'preptime': preptime,
+        'serves': serves,
+        'owner_username': owner_username,
+        'owner_follower': owner_follower,
+        'owner_photo': owner_photo,
+        'ingredient_string': ingredient_string,
+        'steps': steps
+    }
+
+# recipes = database.get_recipes()
+# recipe = recipes.find_one({"title":"testing"})
+# print(recipe_view(recipe['_id']))
+
+'''
+recipe format:
+{
+    'title': 'sdfsfsds' (strip, not empty)
+    'intro':
+    'photo':
+    'sold':
+    'difficulty':
+    'rating':
+    'people_rated':
+    'owner_id':
+    'cooktime':
+    'preptime':
+    'serves':
+    'ingredients':
+        [
+            {
+                'ingredient':
+                'product_id':
+            }
+        ]
+    'steps':
+        [
+            
+        ]
+}
+'''
