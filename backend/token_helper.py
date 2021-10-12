91422bc51ebab53c7b9c8aa733b6fff928302543
TrinaChang <55234114+TrinaChang@users.noreply.github.com>
2021-10-12 23:05:26 +0800

    fix the merge conflict

--- backend/token_helper.py
+++ backend/token_helper.py
@@ -5,10 +5,12 @@ def is_token_valid(token, users):
         else raise an AccessError if the token is not owned by anyone '''
     
     result = users.find_one({"token":token})
+    if token == "":
+        raise AccessError(description='Invalid token')
     if (result is not None):
         return True
     raise AccessError(description='Invalid token')
-    
+
 def invalidate_token(token, users):
     ''' Invalidates a token by setting user token to None '''
     user = users.find_one({"token":token})
