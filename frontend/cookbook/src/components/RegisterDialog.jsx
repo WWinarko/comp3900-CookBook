71d57238390ff1e1d518bbfee33fd85830f2ffc3
Wincent <centwin@hotmail.com>
2021-10-14 06:12:16 +1100

    CHI-1 done

--- frontend/cookbook/src/components/RegisterDialog.jsx
+++ frontend/cookbook/src/components/RegisterDialog.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
+import axios from 'axios';
 import { Dialog,DialogActions, DialogContent, DialogTitle, Stack} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useHistory } from 'react-router-dom';
 
 import CustomTextField from './TextField/CustomTextField';
 import SquareButton from './SquareButton';
+import Notification from './Notification';
 
 
 const dialogTheme = createTheme({
@@ -34,7 +36,6 @@ function RegisterDialog({open, setOpen}) {
     state: '',
     postcode: '',
     phone: '',
-    dob: '',
   }) 
 
   const [accountInfo, setAccountInfo] = useState({
@@ -43,20 +44,78 @@ function RegisterDialog({open, setOpen}) {
     confirmPassword: '',
   });
   const [next, setNext] = useState(false);
+  const [notify, setNotify] = useState({
+    isOpen: false,
+    message: '',
+    type: '',
+  });
   const history = useHistory();
 
   const handleClose = () => {
     setOpen(false);
   }
-
+  const validateInput = () => {
+    let valid = true;
+    for (const [key, value] of Object.entries(personalInfo)) {
+      if (value === '' && ! ["address","state","postcode"].includes(key) ) {
+        setNotify({
+          isOpen: true,
+          message: `${key} field should not be empty`,
+          type: 'error',
+        });
+       valid = false;
+       break;
+      }
+    }
+    if (!valid) {
+      return false;
+    } else {
+      for (const [key, value] of Object.entries(accountInfo)) {
+        if (value === '') {
+          setNotify({
+            isOpen: true,
+            message: `${key} field should not be empty`,
+            type: 'error',
+          });
+         valid = false;
+         break;
+        }
+      }
+    }
+    return valid;
+  };
   const handleRegister = () => {
-    console.log(personalInfo);
-    console.log(accountInfo);
-    history.push('/');
+    if(validateInput()) {
+      axios.post('http://127.0.0.1:5000/auth/register', {
+        first_name: personalInfo['firstName'],
+        last_name:  personalInfo['lastName'],
+        email: personalInfo['email'],
+        address: personalInfo['address'],
+        state:  personalInfo['state'],
+        postcode:  personalInfo['postcode'],
+        phone:  personalInfo['phone'],
+        username: accountInfo['username'],
+        password: accountInfo['password'],
+        confirmpassword: accountInfo['confirmPassword']
+        })
+        .then((res) => {       
+          const {token} = res.data;
+          localStorage.setItem('cookbook-token', token);
+          history.push('/'); 
+        })
+        .catch((err) => {
+          setNotify({
+            isOpen: true,
+            message: err.response.data.message,
+            type: 'error',
+          });
+        })
+    }
   }
   
   return(
     <ThemeProvider theme={dialogTheme}>
+      <Notification notify={notify} setNotify={setNotify} />
       <Dialog 
         open={open} 
         onClose={handleClose}
@@ -72,10 +131,11 @@ function RegisterDialog({open, setOpen}) {
               },
               }}
             >
-              <CustomTextField id="firstName" name="First Name" value={personalInfo['firstName']} setValue={setPersonalInfo}  field="object" width="205px"/>
-              <CustomTextField id="lastName" name="Last Name" value={personalInfo['lastName']} setValue={setPersonalInfo}  field="object" width="205px"/>
+              <CustomTextField id="firstName" required name="First Name" value={personalInfo['firstName']} setValue={setPersonalInfo}  field="object" width="205px"/>
+              <CustomTextField id="lastName" required name="Last Name" value={personalInfo['lastName']} setValue={setPersonalInfo}  field="object" width="205px"/>
             </Stack>
-            <CustomTextField id="email" name="Email" value={personalInfo['email']} setValue={setPersonalInfo}  field="object" width="466px"/>
+            <CustomTextField id="email" required name="Email" value={personalInfo['email']} setValue={setPersonalInfo}  field="object" width="466px"/>
+            <CustomTextField id="phone" required name="Phone" value={personalInfo['phone']} setValue={setPersonalInfo}  field="object" width="466px"/>
             <CustomTextField id="adress" name="Address" value={personalInfo['adress']} setValue={setPersonalInfo}  field="object" width="466px"/>
             <Stack
               direction="row"
@@ -87,26 +147,14 @@ function RegisterDialog({open, setOpen}) {
               <CustomTextField id="state" name="State" value={personalInfo['state']} setValue={setPersonalInfo}  field="object" width="205px"/>
               <CustomTextField id="postcode" name="Postcode" value={personalInfo['postcode']} setValue={setPersonalInfo}  field="object" width="205px"/>
             </Stack>
-            <Stack
-              direction="row"
-              sx={{"div:first-child": {
-                marginRight: '55px',
-              },
-              }}
-            >
-              <CustomTextField id="phone" name="Phone" value={personalInfo['phone']} setValue={setPersonalInfo}  field="object" width="205px"/>
-              <CustomTextField id="dob" name="Date of Birth" value={personalInfo['dob']} setValue={setPersonalInfo}  field="object" width="205px"/>
-            </Stack>
           </> 
         :
           <>
-            <CustomTextField id="username" name="Username" value={accountInfo['username']} setValue={setAccountInfo}  field="object" width="466px"/>
-            <CustomTextField id="password" name="Password" value={accountInfo['password']} setValue={setAccountInfo}  field="object" width="466px" type="password" />
-            <CustomTextField id="confirmPassword" name="Confirm Password" value={accountInfo['confirmPassword']} setValue={setAccountInfo}  field="object" width="466px" type="password" />
+            <CustomTextField id="username" required name="Username" value={accountInfo['username']} setValue={setAccountInfo}  field="object" width="466px"/>
+            <CustomTextField id="password" required name="Password" value={accountInfo['password']} setValue={setAccountInfo}  field="object" width="466px" type="password" />
+            <CustomTextField id="confirmPassword" required name="Confirm Password" value={accountInfo['confirmPassword']} setValue={setAccountInfo}  field="object" width="466px" type="password" />
           </>
         }
-          
-          
         </DialogContent>
         <DialogActions>
             {!next ?
