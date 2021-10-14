2baee8c000f9c0f395e19b2976b0b6e55821a45b
Maria Cuyutupa Garcia <cuyutupamaria@gmail.com>
2021-10-14 17:30:37 +1100

    Names function changed

---
+++ backend/cart_remove.py
@@ -0,0 +1,23 @@
+from error import AccessError
+import token_helper
+import database
+import cart_helper
+
+def cart_remove(token, item):
+    ''' Remove item (only one) from shopping cart '''
+    # Validates token
+    users = database.get_users()
+    token_helper.is_token_valid(token, users)
+
+    # Remove item
+    carts = database.get_carts()
+    cart = carts.find_one({"token": token})
+    ingredient = cart_helper.find_ingredient(cart, item[0])
+    cart["ingredients"].remove(ingredient)
+
+    # If shopping cart empty, then the entry is deleted
+    if len(cart["ingredients"]):
+        cart_id = cart["_id"]
+        carts.update_one({"_id": cart_id}, {"$set": {"ingredients": cart["ingredients"]}})
+    else:
+        carts.delete_one({"token": token})
