32eb47ccf4b6185cc744499f6ba8d5a23e97d19e
Louis Limmarga <louislimmarga@gmail.com>
2021-10-28 03:18:24 +1100

    CHI-80 Added dummy data

---
+++ frontend/cookbook/src/components/MyProfile/TempData.jsx
@@ -0,0 +1,22 @@
+export const OrderData = [
+  {
+    id: 1,
+    status: 'Delivered',
+  },
+  {
+    id: 2,
+    status: 'Dispatched',
+  },
+  {
+    id: 3,
+    status: '',
+  },
+  {
+    id: 4,
+    status: '',
+  },
+  {
+    id: 5,
+    status: '',
+  },
+];
