6243d8d420255eb627a47c275c051b3f1f25ab26
TrinaChang <ks20342@gmail.com>
2021-11-01 20:15:40 +0800

    finished product listall

---
+++ backend/product_listall.py
@@ -0,0 +1,15 @@
+import database
+
+def product_listall():
+    ''' list all the products '''
+    products = database.get_products()
+    product_list = list(products.find())
+    product_ids = []
+
+    for product in product_list:
+        product_ids.append(str(product['_id']))
+    
+    return {
+        "product_list":product_ids
+    }
+    
