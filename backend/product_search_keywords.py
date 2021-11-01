abb4b0b23c4a80eb26f6b5b57cf0fe8e064e80cb
TrinaChang <ks20342@gmail.com>
2021-11-01 20:41:52 +0800

    finished product_search_keywords

---
+++ backend/product_search_keywords.py
@@ -0,0 +1,16 @@
+import database
+import re
+def product_search_keyword(keyword):
+    ''' list all the products with the given keyword '''
+    products = database.get_products()
+    product_list = list(products.find())
+    product_ids = []
+    
+
+    for product in product_list:
+        if re.search(keyword, product['title']):
+            product_ids.append(str(product['_id']))
+
+    return {
+        "product_ids":product_ids
+    }
