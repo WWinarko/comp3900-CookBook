92e15fd18e606724dd1420db87083aa8caaf85a1
Wincent <centwin@hotmail.com>
2021-11-03 22:19:19 +1100

    removed console.log

--- frontend/cookbook/src/components/DeliveryForm.jsx
+++ frontend/cookbook/src/components/DeliveryForm.jsx
@@ -12,7 +12,6 @@ function DeliveryForm({deliveryInfo, setDeliveryInfo}) {
     type: 'error',
   });
   const [save, setSave] = useState(false);
-  console.log(deliveryInfo);
   const validateInput = () => {
     let valid = true;
     if (!valid) {
