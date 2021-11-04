31aaa7042a45e40092d437ca80a9baa655a9bde8
Wincent <centwin@hotmail.com>
2021-11-04 15:00:42 +1100

    CHI-94 DONE

--- frontend/cookbook/src/components/RecommendationContainer.jsx
+++ frontend/cookbook/src/components/RecommendationContainer.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React  from 'react';
 import { makeStyles } from '@mui/styles';
 
-import { RecipeData } from './Recipe/RecipeData';
-import RecommendationCard from './RecommendationCard';
+import HomeRecipeContainer from '../components/HomeRecipeContainer';
 
 const useStyles = makeStyles({
   root: {
     width: '100%',
+    minWidth: '736px',
     display: 'flex',
     justifyContent: 'center',
   },
@@ -20,16 +21,11 @@ const useStyles = makeStyles({
  
 })
 
-function RecommendationContainer() {
+function RecommendationContainer({data}) {
   const classes = useStyles();
-  const recipeData = RecipeData.properties.slice(0,3);
   return (
     <div className={classes.root}>
-      {recipeData.map((recipe, index) => {
-        return (
-          <RecommendationCard data={recipe} key={index}/>
-        )
-      })}
+      <HomeRecipeContainer recipesData={data} />
     </div>
   )
 }
