58fee5124677728c9c085a03dd32e5bcf121dcb6
TrinaChang <ks20342@gmail.com>
2021-11-04 14:11:38 +0800

    add diary and fix bug

--- backend/recipe_comment_view.py
+++ backend/recipe_comment_view.py
@@ -2,7 +2,7 @@ import database
 from bson.objectid import ObjectId
 
 def recipe_comment_view(recipe_id):
-    ''' view comment of the givenn comment_id '''
+    ''' view comment of the given comment_id '''
     recipes = database.get_recipes()
     recipe = recipes.find_one({"_id":ObjectId(recipe_id)})
 
