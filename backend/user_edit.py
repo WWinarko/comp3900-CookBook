3a8c2a448fa43b992b7d74984d5cfedaba59abcd
TrinaChang <ks20342@gmail.com>
2021-11-16 22:14:10 +0800

    add error checking

--- backend/user_edit.py
+++ backend/user_edit.py
@@ -1,12 +1,31 @@
 import database
 import token_helper
-
+import argument_checker
+import auth_helper
+from error import InputError
 def user_edit(token, first_name, last_name, photo, email, address, state, postcode, phone):
     ''' Edit the information of a user '''
     # Retrieve data from the database
     users = database.get_users()
     # check if the token is valid and find the user
     token_helper.is_token_valid(token, users)
+
+    # check if the fields have arguments
+    argument_checker.all_not_empty([first_name, last_name, email, phone])
+
+    # check if the input contains white space
+    argument_checker.no_white_space([first_name, last_name, email, phone])
+    if state != "":
+        argument_checker.no_white_space([state])
+    if postcode != "":
+        argument_checker.no_white_space([postcode])
+
+    # check if the email is in valid and has not been used
+    auth_helper.validate_email(email, users)
+
+    # check if the username has not been taken
+    if auth_helper.check_phone_in_use(phone, users):
+        raise InputError(description="Phone is already taken")
 
     user = users.find_one({"token":token})
 
