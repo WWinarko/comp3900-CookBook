3e0ab6a730fcc34ee58be52bb30a6e27d834a827
Wincent <centwin@hotmail.com>
2021-11-17 17:06:34 +1100

    Added swap ingredient

--- frontend/cookbook/src/components/Recipe/BuyRecipeModalCard.jsx
+++ frontend/cookbook/src/components/Recipe/BuyRecipeModalCard.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
-import { Card, CardContent, CardMedia, Stack, Typography, OutlinedInput } from '@mui/material';
-import CloseSharpIcon from '@mui/icons-material/CloseSharp';
 import axios from 'axios';
+import { Card, CardContent, CardMedia, Stack, Typography, OutlinedInput, Box } from '@mui/material';
+import CloseSharpIcon from '@mui/icons-material/CloseSharp';
+import RoundButton from '../RoundButton';
+import SwapIngredientModal from './SwapIngredientModal';
 
 
-function BuyRecipeModalCard({ id, quantity, removeItem, changeQuantity}) {
+function BuyRecipeModalCard({ id, quantity, removeItem, changeQuantity, swapIngredient}) {
   const [ingredientInfo, setIngredientInfo] = useState({});
-  
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     axios.get('http://127.0.0.1:5000/product/view', {
       params: {product_id: id}
@@ -21,6 +24,10 @@ function BuyRecipeModalCard({ id, quantity, removeItem, changeQuantity}) {
       console.log(err);
     })
   }, [])
+
+  const handleClose = () => {
+    setOpen(!open);
+  }
 
   return (
     <Card sx={{width: '100%', marginTop: '20px', display: 'flex', flexDirection:'row', position: 'relative'}}>
@@ -40,6 +47,9 @@ function BuyRecipeModalCard({ id, quantity, removeItem, changeQuantity}) {
           <Typography variant="h6" sx={{color: '#977554'}}>$ {(ingredientInfo['price'] * quantity).toFixed(2)}</Typography>
         </Stack>
       </CardContent>
+      <Box sx={{alignSelf: 'center'}}>
+        <RoundButton name='Swap' onClick={() => setOpen(true)} />
+      </Box>
       <Stack
         direction="column"
         alignItems="center"
@@ -49,6 +59,7 @@ function BuyRecipeModalCard({ id, quantity, removeItem, changeQuantity}) {
         <OutlinedInput value={quantity} type="number" onChange={(event) => changeQuantity(id, event)} sx={{borderRadius: '3px', width: "50%", backgroundColor:'#ffffff', color: '#000000'}} />
       </Stack>
       <CloseSharpIcon sx={{position: 'absolute', right: 0, padding: '10px', cursor: 'pointer'}} onClick={() => removeItem(id)}/>
+      <SwapIngredientModal open={open} onClose={handleClose} ingredient={ingredientInfo} id={id} swapIngredient={swapIngredient}/>
     </Card>
   )
 }
